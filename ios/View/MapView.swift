a22c4c8796c1f4e716e10df46695c21c91553cb7
nauvalrafli <nauvalrafli@gmail.com>
2026-02-27 15:36:29 +0700

    fix missing text explore by map

--- ios/View/MapView.swift
+++ ios/View/MapView.swift
@@ -1002,21 +1002,19 @@ extension MapView {
             }
         })
         .toolbar(content: {
-            ToolbarItem(placement: .navigation, content: {
-                HStack(spacing: 8, content: {
-                    Image("map-pin-2", bundle: Bundle.mapxus)
-                        .renderingMode(.template)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 26, height: 26)
-                        .foregroundColor(Color.mainColor)
-                    
-                    Text(translationClass.exploreByMap(code: mapxusController.selectedLanguage))
-                        .font(.system(size: 16, weight: .bold))
-                        .foregroundColor(Color.primary)
-                })
-                .frame(maxWidth: .infinity, alignment: .leading)
-            })
+            HStack(spacing: 8, content: {
+                Image("map-pin-2", bundle: Bundle.mapxus)
+                    .renderingMode(.template)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 26, height: 26)
+                    .foregroundColor(Color.mainColor)
+                
+                Text(translationClass.exploreByMap(code: mapxusController.selectedLanguage))
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundColor(Color.primary)
+            }).frame(maxWidth: .infinity, alignment: .leading)
+
         })
     }
     
